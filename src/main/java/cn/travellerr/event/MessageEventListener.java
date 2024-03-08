a5c4df21dcc28c54c0807cdb381f9e3021162d42
Travellerrr <chenshaoyi11@hotmail.com>
2024-03-08 20:36:24 +0800

    更新: 今日运势功能增加@触发者，方便区分

--- src/main/java/cn/travellerr/event/MessageEventListener.java
+++ src/main/java/cn/travellerr/event/MessageEventListener.java
@@ -3,10 +3,10 @@ package cn.travellerr.event;
 import cn.travellerr.BlueArchive.jrys;
 import cn.travellerr.GehshinHelp.CharacterHelp;
 import cn.travellerr.tools.Log;
-
 import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.SimpleListenerHost;
+import net.mamoe.mirai.event.events.GroupMessageEvent;
 import net.mamoe.mirai.event.events.MessageEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -29,16 +29,26 @@ public class MessageEventListener extends SimpleListenerHost {
             case "#原神角色列表":
                 Contact.sendImage(subject, new File("./data/cn.travellerr.GenshinHelper/GenshinHelp/角色列表/info.png"));
                 return;
-            case "#今日运势":
-            case "#jrys":
-                jrys.info(event);
-                Log.info("运势指令");
-                return;
+
         }
         String info = "#原神攻略 (\\S+)";
         if (Pattern.matches(info, msg)) {
             Log.info("攻略指令");
             CharacterHelp.help(event);
+        }
+
+
+    }
+
+    @EventHandler
+    public void onGroupMessgae(@NotNull GroupMessageEvent event) {
+        Contact subject = event.getSubject();
+        String msg = event.getMessage().serializeToMiraiCode();
+        switch (msg) {
+            case "#今日运势":
+            case "#jrys":
+                jrys.info(event);
+                Log.info("运势指令");
         }
     }
 }
