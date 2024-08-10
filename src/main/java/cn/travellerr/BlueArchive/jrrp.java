a7278699a18fed0a12d84a74ebbb486ea3be613f
Travellerrr <chenshaoyi11@hotmail.com>
2024-08-10 13:34:16 +0800

    修复：新创建数据库使用“今日人品”找不到表问题

--- src/main/java/cn/travellerr/BlueArchive/jrrp.java
+++ src/main/java/cn/travellerr/BlueArchive/jrrp.java
@@ -9,20 +9,18 @@ public class jrrp {
     public static void info(Contact subject, User user) {
         long qqNumber = user.getId();
         boolean isUserNew = GetSentenceApi.isQQNumberNew(qqNumber);
-        boolean isDifferentDate = false;
-        if (!isUserNew) {
-            isDifferentDate = GetSentenceApi.isDateDifferent(qqNumber);
-        }
+        boolean isDifferentDate = !isUserNew && GetSentenceApi.isDateDifferent(qqNumber);
 
         MessageChainBuilder message = new MessageChainBuilder();
         message.append(new At(qqNumber)).append("\n");
-        int jrrp;
+
         if (!isDifferentDate && !isUserNew) {
             message.append("今日已查询,");
         } else {
             GetSentenceApi.generateFortuneID(qqNumber, false);
         }
-        jrrp = GetSentenceApi.getFortuneID(qqNumber) % 101;
+
+        int jrrp = GetSentenceApi.getFortuneID(qqNumber) % 101;
         message.append("您的今日人品为:").append(String.valueOf(jrrp));
         subject.sendMessage(message.build());
     }
